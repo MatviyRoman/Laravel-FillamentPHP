48e6b0b2c824f910109167654e75f8a0e62f050f
Roman Matviy <roman@matviy.pp.ua>
2024-08-13 19:37:03 +0300

    Add task seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            ProjectsTableSeeder::class
+            ProjectsTableSeeder::class,
+            TasksTableSeeder::class,
         ]);
     }
 }
